1983043a30c480d594d9e2506dbff188fd2bf6f2
Oleg Tyutyunnikov <negativelight@gmail.com>
2021-01-15 11:16:04 +0300

    Added the Unity project

---
+++ rav3_sound_design/Assets/Jump.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    private FMOD.Studio.EventInstance instance;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            instance = FMODUnity.RuntimeManager.CreateInstance("event:/Character/Jump");
+            instance.start();
+        }
+    }
+}
---
+++ rav3_sound_design/Assets/Scripts/footsteps.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class footsteps : MonoBehaviour
+{
+    private FMOD.Studio.EventInstance instance;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D))
+        {
+            instance = FMODUnity.RuntimeManager.CreateInstance("event:/Character/Footsteps");
+            instance.start();
+        }
+
+        if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.D))
+        {
+            _ = instance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+            _ = instance.release();
+        }
+    }
+}
